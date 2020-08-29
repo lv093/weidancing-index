a1df12efb0db654298e0cc276145c7a30fbfd9b0
lv <736598912@qq.com>
2020-08-29 20:23:48 +0800

    baidu & youmeng statistic

---
+++ resource/js/baidu_stat.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function () {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?3db5d0b8f5edf15141a643b728edb2f1";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
